a29f975610c6bf2e77795561305dfe44fdb6d5d6
John Wilkos <portablejohn@gmail.com>
2015-06-04 00:46:50 +0200

    got it up and working and buggy!

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
 
 $(document).ready(function(){
 
-	var boxNum = 16;
+	var boxNum = 64;
 	var makeNew = $("#makeNew");
 	var newEntry = $("#newEntry");
 	var newSize = $("#newSize");
+	var innCon = $("#innerContainer");
+
+	var fillGrid = function(input){
+		var size = 512/input;
+		innCon.empty();
+		for(i=0;i<(input*input);i++){
+			innCon.append("<div class='square gray' style='height: "+ size +"px; width:"+ size +"px;'></div>");
+		}
+		$(".square").hover(function(){
+			$(this).removeClass("gray").addClass("black");
+		});
+	};
+
+	fillGrid(boxNum);
 
 	makeNew.on("click",function(event){
 		event.preventDefault();
 		$(this).hide();
+		newSize.val("");
 		newEntry.fadeIn("slow", function(){
-			newSize.val("");
 			newSize.focus();
 		});
 	});
@@ -23,7 +37,8 @@ $(document).ready(function(){
 	$("#finished").on("click", function(event){
 		event.preventDefault();
 		boxNum = newSize.val();
-		if($.isNumeric(boxNum) && boxNum > 0 && boxNum <= 100){
+		if($.isNumeric(boxNum) && boxNum > 0 && boxNum <= 10000){
+			fillGrid(boxNum);
 			newEntry.fadeOut("slow", function(){
 				makeNew.fadeIn();
 			});
@@ -35,4 +50,6 @@ $(document).ready(function(){
 			newSize.focus();
 		}
 	});
+
+
 });
